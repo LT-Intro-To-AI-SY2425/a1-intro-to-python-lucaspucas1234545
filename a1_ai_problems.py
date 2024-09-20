eb16a3e586bac90c3ce217f3803c7df7716cacca
lucaspucas1234545 <lkstuckey@cps.edu>
2024-09-20 10:36:58 -0500

    added 3 functions to a1 ai problems

--- a1_ai_problems.py
+++ a1_ai_problems.py
@@ -17,3 +17,35 @@ assert fizbuzz(5) == "Buzz", "fizzbuzz 5 test"
 assert fizbuzz(6) == "Fizz", "fizzbuzz 6 test"
 assert fizbuzz(15) == "FizzBuzz", "fizzbuzz 15 test"
 
+def temp(input_temp, input_type):
+    if input_type == "F":
+        val = (input_temp - 32) * (5 / 9)
+    else:
+        val = (input_temp * (9 / 5)) + 32
+
+    print(val)
+    return val
+
+assert temp(32, "F") == 0, "temp 32 degrees F"
+assert temp(100, "C") == 212, "temp 0 degrees C"
+
+def wordCounter(input_string):
+    i = 0
+    numWords = 1
+    for char in input_string:
+        i += 1
+        if char == " ":
+            numWords +=1
+    
+    return numWords
+
+assert wordCounter("sho fidda bruh moment") == 4, "4 word string"
+assert wordCounter("crazy") == 1, "1 word string"
+
+def numGuesser(correct_num):
+    guess = int(input("Enter your guess "))
+    if guess == correct_num:
+        return True
+    else:
+        return False
+    
